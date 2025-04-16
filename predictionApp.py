e93451c5ef396d528539976229a164726efba43c
KareemAdelFaheem <2021170404@cis.asu.edu.eg>
2025-04-16 11:49:59 +0200

    editing return value

--- predictionApp.py
+++ predictionApp.py
@@ -92,11 +92,11 @@ def predict():
     final_data_json = final_data.tolist() if isinstance(final_data, np.ndarray) else final_data.to_json(
         orient='records')
     prediction= model.predict(final_data)
-    # return jsonify({
-    #     "status": "success",
-    #     "final_data": final_data_json,
-    #     "prediction": float(prediction[0])
-    # })
+    return jsonify({
+        "status": "success",
+        "final_data": final_data_json,
+        "prediction": float(prediction[0])
+    })
 
     # weather_data = get_weather()
     # print(weather_data["current"]["cloud_cover"])
